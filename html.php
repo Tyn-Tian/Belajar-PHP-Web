3a492fef8228ea8e0fcf66fc32532b2ad50e977d
lukxnut <kristian2015ok@gmail.com>
2024-04-24 11:22:07 +0700

    Add: Integrasi dengan HTML

---
+++ html.php
@@ -0,0 +1,18 @@
+<?php
+
+$title = "Hello PHP dan HTML";
+$body = "Hello PHP dan HTML";
+?>
+
+<html>
+    <head>
+        <title>
+            <?= $title ?>
+        </title>
+    </head>
+    <body>
+        <h1>
+            <?= $body?>
+        </h1>
+    </body>
+</html>
